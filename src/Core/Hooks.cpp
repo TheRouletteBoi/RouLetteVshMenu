0c6ebca8b4f2e203eb61f77c0e876c929c65284a
TheRouletteBoi <therouletteboi@outlook.com>
2022-04-16 04:56:37 -0700

    Remove CCAPI Patch

--- src/Core/Hooks.cpp
+++ src/Core/Hooks.cpp
@@ -14,27 +14,8 @@ void pafFrameworkRenderHook(void* framework, float frameTime)
    g_Overlay.OnUpdate();
 }
 
-void RemoveCCAPIHooks()
-{
-    opd_s* ccapiDetourPaf_85D1D23B = FindExportByName("paf", 0x85D1D23B);
-    opd_s* ccapiDetourSysio_3733EA3C = FindExportByName("sys_io", 0x3733EA3C);
-
-    if (ccapiDetourPaf_85D1D23B && ccapiDetourSysio_3733EA3C)
-    {
-        // Checks if the exports haven't been detoured by CCAPI to avoid crashing
-        if (VshGetMem<uint32_t>(ccapiDetourPaf_85D1D23B->func + 0x14) != 0xF80100D0)
-            VshSetMem<uint32_t>(ccapiDetourPaf_85D1D23B->func + 0x14, 0xF80100D0);
-
-        if (VshGetMem<uint32_t>(ccapiDetourSysio_3733EA3C->func + 0x0C) != 0xF8010080) // || 0x4E800420 
-            VshSetMem<uint32_t>(ccapiDetourSysio_3733EA3C->func + 0x0C, 0xF8010080);
-    }
-}
-
 void InstallHooks()
 {
-   if (!IsConsoleHen())
-      RemoveCCAPIHooks();
-
    pafFrameworkRenderHk = new ImportExportDetour(ImportExportDetour::Export, "paf", 0x85D1D23B, (uintptr_t)pafFrameworkRenderHook);
 }
 
