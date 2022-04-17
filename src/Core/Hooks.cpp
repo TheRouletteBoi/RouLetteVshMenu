a5c6ec8d0e1d8df57764f7c3e3e938232321dbd4
TheRouletteBoi <therouletteboi@outlook.com>
2022-04-17 14:28:47 -0700

    Change framework hook to allow compatibility with CCAPI

--- src/Core/Hooks.cpp
+++ src/Core/Hooks.cpp
@@ -5,12 +5,12 @@
 #include <vsh/stdc.h>
 #include <cell/pad/pad_codes.h>
 
-ImportExportDetour* pafFrameworkUpdateHk;
+ImportExportDetour* pafFrameworkBeginHookHk;
 Detour* cellPadGetDataExtraInternalHk;
 
-void pafFrameworkUpdateHook(void* framework, float frameTime)
+void pafFrameworkBeginHook(void* framework, float frameTime)
 {
-   pafFrameworkUpdateHk->GetOriginal<void>(framework, frameTime);
+    pafFrameworkBeginHookHk->GetOriginal<void>(framework, frameTime);
 
    g_Helpers.OnUpdate();
    g_Render.OnUpdate();
@@ -20,24 +20,22 @@ void pafFrameworkUpdateHook(void* framework, float frameTime)
 
 int cellPadGetDataExtraInternalHook(void* r3, uint32_t portNum, uint32_t* deviceType, CellPadData* data, bool canReturnWithoutClearing)
 {
-   if (portNum != 0)
-      return cellPadGetDataExtraInternalHk->GetOriginal<int>(r3, portNum, deviceType, data, canReturnWithoutClearing);
+   int returnValue = cellPadGetDataExtraInternalHk->GetOriginal<int>(r3, portNum, deviceType, data, canReturnWithoutClearing);
+   if (portNum)
+       return returnValue;
 
-   int returnValue = cellPadGetDataExtraInternalHk->GetOriginal<int>(r3, portNum, deviceType, g_Input.GetInputData(), canReturnWithoutClearing);
-   vsh::memcpy(data, g_Input.GetInputData(), sizeof(CellPadData));
+   g_Input.SetInputData(data);
 
    if (g_Menu.IsOpened())
    {
-      // Clear button flags
-      data->button[CELL_PAD_BTN_OFFSET_DIGITAL1] &= ~(CELL_PAD_CTRL_LEFT | CELL_PAD_CTRL_DOWN | CELL_PAD_CTRL_RIGHT | CELL_PAD_CTRL_UP);
-      data->button[CELL_PAD_BTN_OFFSET_DIGITAL2] &= ~(CELL_PAD_CTRL_CROSS | CELL_PAD_CTRL_CIRCLE);
+       data->button[CELL_PAD_BTN_OFFSET_DIGITAL1] &= ~(CELL_PAD_CTRL_LEFT | CELL_PAD_CTRL_DOWN | CELL_PAD_CTRL_RIGHT | CELL_PAD_CTRL_UP);
+       data->button[CELL_PAD_BTN_OFFSET_DIGITAL2] &= ~(CELL_PAD_CTRL_CROSS | CELL_PAD_CTRL_CIRCLE);
 
-      if (g_Menu.IsMoving())
-      {
-         // Clear analog values
-         data->button[CELL_PAD_BTN_OFFSET_ANALOG_RIGHT_X] = 128;
-         data->button[CELL_PAD_BTN_OFFSET_ANALOG_RIGHT_Y] = 128;
-      }
+       if (g_Menu.IsMoving())
+       {
+           data->button[CELL_PAD_BTN_OFFSET_ANALOG_RIGHT_X] = 128;
+           data->button[CELL_PAD_BTN_OFFSET_ANALOG_RIGHT_Y] = 128;
+       }
    }
 
    return returnValue;
@@ -45,12 +43,12 @@ int cellPadGetDataExtraInternalHook(void* r3, uint32_t portNum, uint32_t* device
 
 void InstallHooks()
 {
-   pafFrameworkUpdateHk = new ImportExportDetour(ImportExportDetour::Export, "paf", 0x85D1D23B, (uintptr_t)pafFrameworkUpdateHook);
+   pafFrameworkBeginHookHk = new ImportExportDetour(ImportExportDetour::Export, "paf", 0x59BDA198, (uintptr_t)pafFrameworkBeginHook);
    cellPadGetDataExtraInternalHk = new Detour(ResolveBranch(FindExportByName("sys_io", 0x3733EA3C)->func + 0x6C), (uintptr_t)cellPadGetDataExtraInternalHook);
 }
 
 void RemoveHooks()
 {
-   delete pafFrameworkUpdateHk;
+   delete pafFrameworkBeginHookHk;
    delete cellPadGetDataExtraInternalHk;
 }
