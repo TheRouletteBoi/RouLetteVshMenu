9cd020a747b2e395cbcd32593f9bb3da9db451c3
TheRouletteBoi <therouletteboi@outlook.com>
2022-04-30 18:28:47 -0700

    😂

--- src/Memory/Pattern.cpp
+++ src/Memory/Pattern.cpp
@@ -1,6 +1,24 @@
 #include "Pattern.hpp"
 #include <sys/memory.h>
 
+uint32_t ReadHighLow(uint32_t address, uint32_t highAdditive, uint32_t lowAdditive)
+{
+	uint32_t returnAddr = (((uint16_t)(*(uint32_t*)(address + highAdditive)) << 16) | (uint16_t)(*(uint32_t*)(address + lowAdditive)));
+	uint32_t returnFinal = (returnAddr & 0x8000) ? returnAddr - 0x10000 : returnAddr;
+	return returnFinal;
+}
+
+uint32_t ResolveBranch(uint32_t branchAddress)
+{
+	uint32_t instruction = *(uint32_t*)(branchAddress);
+	int32_t offset = instruction & 0x3FFFFFC;
+
+	if (offset & (1 << 25))
+		offset |= ~0x03FFFFFF;
+
+	return branchAddress + offset;
+}
+
 bool DataCompare(const uint8_t* pbData, const uint8_t* pbMask, const char* szMask)
 {
     for (; *szMask; ++szMask, ++pbData, ++pbMask)
@@ -29,24 +47,6 @@ uintptr_t FindPatternVshInTextSegment(uint8_t* bytes, const char* mask)
         return 0;
 
     return address;
-}
-
-uint32_t ReadHighLow(uint32_t address, uint32_t highAdditive, uint32_t lowAdditive)
-{
-    uint32_t returnAddr = (((uint16_t)(*(uint32_t*)(address + highAdditive)) << 16) | (uint16_t)(*(uint32_t*)(address + lowAdditive)));
-    uint32_t returnFinal = (returnAddr & 0x8000) ? returnAddr - 0x10000 : returnAddr;
-    return returnFinal;
-}
-
-uint32_t ResolveBranch(uint32_t branchAddress)
-{
-    uint32_t instruction = *(uint32_t*)(branchAddress);
-    int32_t offset = instruction & 0x3FFFFFC;
-
-    if (offset & (1 << 25))
-        offset |= ~0x03FFFFFF;
-
-    return branchAddress + offset;
 }
 
 uint32_t FindPatternHypervisor(uint32_t startAddress, uint32_t stopAddress, uint8_t step, const char* sfind, size_t len, const char* mask)
--- src/Memory/Pattern.hpp
+++ src/Memory/Pattern.hpp
@@ -9,11 +9,11 @@
 constexpr uintptr_t gBaseAddress = 0x00010200;    // first sub in the eboot in ida
 constexpr size_t gInfo_SizeOfImage = 0x008FFFFF;  // open the eboot in ida then go all the way to the end and then added FFFF to the last bytes
 
+uint32_t ReadHighLow(uint32_t address, uint32_t highAdditive, uint32_t lowAdditive);
+uint32_t ResolveBranch(uint32_t branchAddress);
 bool DataCompare(const uint8_t* pbData, const uint8_t* pbMask, const char* szMask);
 uintptr_t FindPatternVsh(uintptr_t address, uint32_t length, uint8_t* bytes, const char* mask);
 uintptr_t FindPatternVshInTextSegment(uint8_t* bytes, const char* mask);
-uint32_t ReadHighLow(uint32_t address, uint32_t highAdditive, uint32_t lowAdditive);
-uint32_t ResolveBranch(uint32_t branchAddress);
 
 
 uint32_t FindPatternHypervisor(uint32_t startAddress, uint32_t stopAddress, uint8_t step, const char* sfind, size_t len, const char* mask);
