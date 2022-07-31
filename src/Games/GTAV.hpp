c99bda2df8b47fc221f15496e81b951f11ffcb01
TheRouletteBoi <therouletteboi@outlook.com>
2022-07-31 00:55:08 -0700

    Ida2Mem for all GTAV menus

--- src/Games/GTAV.hpp
+++ src/Games/GTAV.hpp
@@ -10,55 +10,62 @@ namespace GTAV
       switch (g_FindActiveGame.m_MenuToLoad)
       {
          case CFindActiveGame::PatchedMenu::GTAVLexicon:
-            SetBranch(Ida2Mem("Lexicon", 0x57BC0), Ida2Mem("Lexicon", 0x57C5C));  // so you can load the menu offline
-            SetNop(Ida2Mem("Lexicon", 0x561E8));                                  // nop read license key
-            SetBranch(Ida2Mem("Lexicon", 0x56200), Ida2Mem("Lexicon", 0x57AE8));  // skip to hooking
-            SetNop(Ida2Mem("Lexicon", 0x57B28));                                  // nop address decrypter
-            SetNop(Ida2Mem("Lexicon", 0x57B3C));                                  // nop thread for server settings
-            SetNop(Ida2Mem("Lexicon", 0x57B40));                                  // nop thread for server settings 2
-            SetMem<uint32_t>(0x1C2BB80, 0x2520878);         // hook is player online
-            //SetMem<uint32_t>(0x1C271E0, 0x2520878);       // hook GET_INDEX_OF_CURRENT_LEVEL  an old hook he used in his previous versions of lexicon
-            SetMem<uint32_t>(0x1C2A3D0, 0x2520DEC);         // hook disable control actions
-            SetMem<uint32_t>(0x1C2E9F8, 0x2520EFC);         // hook set ped current weapon
-            vsh::ShowNofityWithSound(L"Lexicon has been patched", vsh::eNotifyIcon::Pen, vsh::eNotifySound::Trophy);
-            break;
+         {
+             const char* sprxInternalName = "Lexicon";
+             SetBranch(Ida2Mem(sprxInternalName, 0x57BC0), Ida2Mem(sprxInternalName, 0x57C5C)); // so you can load the menu offline
+             SetNop(Ida2Mem(sprxInternalName, 0x561E8));                                        // nop read license key
+             SetBranch(Ida2Mem(sprxInternalName, 0x56200), Ida2Mem(sprxInternalName, 0x57AE8)); // skip to hooking
+             SetNop(Ida2Mem(sprxInternalName, 0x57B28));                                        // nop address decrypter
+             SetNop(Ida2Mem(sprxInternalName, 0x57B3C));                                        // nop thread for server settings
+             SetNop(Ida2Mem(sprxInternalName, 0x57B40));                                        // nop thread for server settings 2
+             SetMem<uint32_t>(0x1C2BB80, Ida2Mem(sprxInternalName, 0x50878));                   // hook is player online
+             //SetMem<uint32_t>(0x1C271E0, Ida2Mem(sprxInternalName, 0x50878));                 // hook GET_INDEX_OF_CURRENT_LEVEL  an old hook he used in his previous versions of lexicon
+             SetMem<uint32_t>(0x1C2A3D0, Ida2Mem(sprxInternalName, 0x50DEC));                   // hook disable control actions
+             SetMem<uint32_t>(0x1C2E9F8, Ida2Mem(sprxInternalName, 0x50EFC));                   // hook set ped current weapon
+             vsh::ShowNofityWithSound(L"Lexicon has been patched", vsh::eNotifyIcon::Pen, vsh::eNotifySound::Trophy);
+             break;
+         }
          case CFindActiveGame::PatchedMenu::GTAVSerendipity:
          {
-            SetNop(0x24D4194);                        // read key | 0x24D3968
-            SetNop(0x24D41D0);                        // first auth request
-            SetNop(0x24D42B0);                        // seconds auth request
-            SetMem<uint32_t>(0x2530064, 0x3E3A20);    // Main Hook Address 
-            SetMem<uint32_t>(0x2531B40, 0x3F30A3D7);  // Text Position X
-            SetMem<uint32_t>(0x2531B44, 0x3F4CCCCD);  // Text Position Y
+             const char* sprxInternalName = "GTA5_Menu";
+             SetNop(Ida2Mem(sprxInternalName, 0x4194));                         // read key | 0x3968
+             SetNop(Ida2Mem(sprxInternalName, 0x41D0));                         // first auth request
+             SetNop(Ida2Mem(sprxInternalName, 0x42B0));                         // seconds auth request
+             SetMem<uint32_t>(Ida2Mem(sprxInternalName, 0x60064), 0x3E3A20);    // Main Hook Address 
+             SetMem<uint32_t>(Ida2Mem(sprxInternalName, 0x61B40), 0x3F30A3D7);  // Text Position X
+             SetMem<uint32_t>(Ida2Mem(sprxInternalName, 0x61B44), 0x3F4CCCCD);  // Text Position Y
 
-            uint32_t menu_rgba[] = {
-               0x000000FF, 0x000000FF, 0x000000FF, 0x000000FF, 0x000000FF, 0x000000FF,
-               0x000000FF, 0x000000FF, 0x0000000F, 0x0000000F, 0x0000000F, 0x00000078,
-               0x000000FF, 0x0000007D, 0x00000000, 0x000000AF, 0x000000FF, 0x0000007D,
-               0x00000000, 0x000000AF, 0x000000FF, 0x0000007D, 0x00000000, 0x000000AF
-            };
-            WriteProcessMemory(g_FindActiveGame.GetRunningGameProcessId(), (void*)0x2531C10, &menu_rgba, sizeof(menu_rgba));
+             uint32_t menu_rgba[] = {
+                0x000000FF, 0x000000FF, 0x000000FF, 0x000000FF, 0x000000FF, 0x000000FF,
+                0x000000FF, 0x000000FF, 0x0000000F, 0x0000000F, 0x0000000F, 0x00000078,
+                0x000000FF, 0x0000007D, 0x00000000, 0x000000AF, 0x000000FF, 0x0000007D,
+                0x00000000, 0x000000AF, 0x000000FF, 0x0000007D, 0x00000000, 0x000000AF
+             };
+             WriteProcessMemory(g_FindActiveGame.GetRunningGameProcessId(), (void*)Ida2Mem(sprxInternalName, 0x61C10), &menu_rgba, sizeof(menu_rgba));
 
-            vsh::ShowNofityWithSound(L"Serendipity has been patched", vsh::eNotifyIcon::Pen, vsh::eNotifySound::Trophy);
-            break;
+             vsh::ShowNofityWithSound(L"Serendipity has been patched", vsh::eNotifyIcon::Pen, vsh::eNotifySound::Trophy);
+             break;
          }
          case CFindActiveGame::PatchedMenu::GTAVTerrorizer:
-            SetNop(Ida2Mem("GTAOTerrorizer", 0x4FD58));                       // nop auther thread
-            SetNop(Ida2Mem("GTAOTerrorizer", 0x48B40));                       // bypass check in PatchInJump
-            SetMem<uint32_t>(Ida2Mem("GTAOTerrorizer", 0x48D74), 0x2C030001); // set some check to true
-            SetMem<uint32_t>(Ida2Mem("GTAOTerrorizer", 0x48E48), 0x38600001); // set check to true
-            SetBranchless(Ida2Mem("GTAOTerrorizer", 0x48BF4));                // branch where he sets up all the hooks
-            vsh::ShowNofityWithSound(L"Terrorizer has been patched", vsh::eNotifyIcon::Pen, vsh::eNotifySound::Trophy);
-            break;
+         {
+             const char* sprxInternalName = "GTAOTerrorizer";
+             SetNop(Ida2Mem(sprxInternalName, 0x4FD58));                       // nop auther thread
+             SetNop(Ida2Mem(sprxInternalName, 0x48B40));                       // bypass check in PatchInJump
+             SetMem<uint32_t>(Ida2Mem(sprxInternalName, 0x48D74), 0x2C030001); // set some check to true
+             SetMem<uint32_t>(Ida2Mem(sprxInternalName, 0x48E48), 0x38600001); // set check to true
+             SetBranchless(Ida2Mem(sprxInternalName, 0x48BF4));                // branch where he sets up all the hooks
+             vsh::ShowNofityWithSound(L"Terrorizer has been patched", vsh::eNotifyIcon::Pen, vsh::eNotifySound::Trophy);
+             break;
+         }
          case CFindActiveGame::PatchedMenu::GTAVDebugPayload:
          {
-            // wait because injecting menu too fast can result in a crash
-            Sleep(50000);
+             // wait because injecting menu too fast can result in a crash
+             Sleep(50000);
 
-            const std::string& fileName = GetCurrentDir() + "modmenus/GTAV/GTAVPayload.bin";
-            if (StartPayload(fileName.c_str(), KB(4), 0x7D0, 0x4000))
-               vsh::ShowNofityWithSound(L"GTAV Debug Payload is now loaded", vsh::eNotifyIcon::Pen, vsh::eNotifySound::Trophy);
-            break;
+             const std::string& fileName = GetCurrentDir() + "modmenus/GTAV/GTAVPayload.bin";
+             if (StartPayload(fileName.c_str(), KB(4), 0x7D0, 0x4000))
+                 vsh::ShowNofityWithSound(L"GTAV Debug Payload is now loaded", vsh::eNotifyIcon::Pen, vsh::eNotifySound::Trophy);
+             break;
          }
       }
    }
