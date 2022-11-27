f4dca4525c32bb4e7579a41d24132871321a1821
TheRouLetteBoi <therouletteboi@outlook.com>
2022-11-27 04:46:45 -0700

    Use base address for paradox bo2 patches

--- src/Games/CODBO2.hpp
+++ src/Games/CODBO2.hpp
@@ -11,48 +11,54 @@ namespace CODBO2
         {
             case FindActiveGame::PatchedMenu::BO2Paradox:
             {
-                SetMem<uint32_t>(0x30FADB8, 0x38600001);            // read license key return true
+                // this whole code for paradox bo2 effectively does nothing as it's not fast enough to inject before module is started. To fix this we needed to implement the code into a sprx and then #load, #patch, #start sprx.
+                const char* sprxInternalName = "ParadoxCE";
+                if (IsModuleLoaded(sprxInternalName))
+                {
+                    SetMem<uint32_t>(Ida2Mem(sprxInternalName, 0xADB8), 0x38600001);            // read license key return true
 
-                uint32_t license_and_auth_vars[] = { 0x1CC1D99A, 0xD0025C50, 0x001337EA, 0x01000000 };
-                WriteProcessMemory(g_FindActiveGame.GetRunningGameProcessId(), (void*)0x3140E40, &license_and_auth_vars, sizeof(license_and_auth_vars));
+                    uint32_t license_and_auth_vars[] = { 0x1CC1D99A, 0xD0025C50, 0x001337EA, 0x01000000 };
+                    WriteProcessMemory(g_FindActiveGame.GetRunningGameProcessId(), (void*)Ida2Mem(sprxInternalName, 0x50E40), &license_and_auth_vars, sizeof(license_and_auth_vars));
 
-                SetNop(0x30FAF5C);                                  // stop connection to paradox.sh
+                    SetNop(Ida2Mem(sprxInternalName, 0xAF5C));                                  // stop connection to paradox.sh
 
-                // addresses loaded from server
-                uint32_t xor_addresses[] = {
-                    0x1C175042, 0x1C23F682, 0x1C23F686, 0x1C23F6BA, 0x1CC5F652, 0x1CD0BCBE,
-                    0x1CB7EBBA, 0x1CB47B5A, 0x1CB77A72, 0x1CB7708A, 0x1C841702, 0x1C84175A,
-                    0x1CC08012, 0x1CC08E92, 0x1C8934F2, 0x1CC54EBA, 0x1CF0E582, 0x1CB77DDE,
-                    0x1CC77402, 0x1CC77456, 0x1CD10D8A, 0x1CFC70D2, 0x1CFC7FB2, 0x1CB47AA2,
-                    0x1CB47C4A, 0x1C9ED052, 0x1C87F7AE, 0x1CC58F62
-                };
-                uint32_t malloc_0x70_address = GetMem<uint32_t>(0x3130094);
-                if (malloc_0x70_address != 0)
-                    WriteProcessMemory(g_FindActiveGame.GetRunningGameProcessId(), (void*)malloc_0x70_address, &xor_addresses, sizeof(xor_addresses));
+                    // addresses loaded from server
+                    uint32_t xor_addresses[] = {
+                        0x1C175042, 0x1C23F682, 0x1C23F686, 0x1C23F6BA, 0x1CC5F652, 0x1CD0BCBE,
+                        0x1CB7EBBA, 0x1CB47B5A, 0x1CB77A72, 0x1CB7708A, 0x1C841702, 0x1C84175A,
+                        0x1CC08012, 0x1CC08E92, 0x1C8934F2, 0x1CC54EBA, 0x1CF0E582, 0x1CB77DDE,
+                        0x1CC77402, 0x1CC77456, 0x1CD10D8A, 0x1CFC70D2, 0x1CFC7FB2, 0x1CB47AA2,
+                        0x1CB47C4A, 0x1C9ED052, 0x1C87F7AE, 0x1CC58F62
+                    };
+                    uint32_t malloc_0x70_address = GetMem<uint32_t>(Ida2Mem(sprxInternalName, 0x40094));
+                    if (malloc_0x70_address != 0)
+                        WriteProcessMemory(g_FindActiveGame.GetRunningGameProcessId(), (void*)malloc_0x70_address, &xor_addresses, sizeof(xor_addresses));
 
-                // offsets for 'malloc_0x70' structure
-                uint32_t server_data[] = {
-                    0x00000000, 0x00000004, 0x00000008, 0x0000000C, 0x00000010, 0x00000000,
-                    0x00000014, 0x00000000, 0x00000018, 0x00000000, 0x0000001C, 0x00000000,
-                    0x00000020, 0x00000000, 0x00000024, 0x00000000, 0x00000028, 0x00000000,
-                    0x0000002C, 0x00000000, 0x00000030, 0x00000000, 0x00000034, 0x00000000,
-                    0x00000038, 0x00000000, 0x0000003C, 0x00000000, 0x00000040, 0x00000000,
-                    0x00000044, 0x00000000, 0x00000048, 0x00000000, 0x0000004C, 0x00000000,
-                    0x00000050, 0x00000000, 0x00000054, 0x00000000, 0x00000058, 0x00000000,
-                    0x0000005C, 0x00000000, 0x00000060, 0x00000000, 0x00000064, 0x00000000,
-                    0x00000068, 0x00000000, 0x0000006C
-                };
-                WriteProcessMemory(g_FindActiveGame.GetRunningGameProcessId(), (void*)0x3150EF0, &server_data, sizeof(server_data));
+                    // offsets for 'malloc_0x70' structure
+                    uint32_t server_data[] = {
+                        0x00000000, 0x00000004, 0x00000008, 0x0000000C, 0x00000010, 0x00000000,
+                        0x00000014, 0x00000000, 0x00000018, 0x00000000, 0x0000001C, 0x00000000,
+                        0x00000020, 0x00000000, 0x00000024, 0x00000000, 0x00000028, 0x00000000,
+                        0x0000002C, 0x00000000, 0x00000030, 0x00000000, 0x00000034, 0x00000000,
+                        0x00000038, 0x00000000, 0x0000003C, 0x00000000, 0x00000040, 0x00000000,
+                        0x00000044, 0x00000000, 0x00000048, 0x00000000, 0x0000004C, 0x00000000,
+                        0x00000050, 0x00000000, 0x00000054, 0x00000000, 0x00000058, 0x00000000,
+                        0x0000005C, 0x00000000, 0x00000060, 0x00000000, 0x00000064, 0x00000000,
+                        0x00000068, 0x00000000, 0x0000006C
+                    };
+                    WriteProcessMemory(g_FindActiveGame.GetRunningGameProcessId(), (void*)Ida2Mem(sprxInternalName, 0x60EF0), &server_data, sizeof(server_data));
 
-                SetNop(0x30FBB2C);                                  // update module
-                SetBranch(0x30FADC4, 0x30FAEEC);                    // skip the bullshit
-                SetBranch(0x30FAF60, 0x30FB800);                    // skip the bullshit
-                SetBranch(0x30FAB04, 0x30FAD40);                    // skip the bullshit
-                SetBranch(0x312525C, 0x31255B0);                    // skip everything except for malloc
-                SetMem<uint32_t>(0x10021594, 0xDE7BBE46);           // xor value 2
-                SetBranch(0x311DBAC, 0x311DC24);                    // skip thread exit. ##Crashes sometimes if load too quickly or too slowly
+                    SetNop(Ida2Mem(sprxInternalName, 0xBB2C));                                  // update module
+                    SetBranch(Ida2Mem(sprxInternalName, 0xADC4), Ida2Mem(sprxInternalName, 0xAEEC));                    // skip the bullshit
+                    SetBranch(Ida2Mem(sprxInternalName, 0xAF60), Ida2Mem(sprxInternalName, 0xB800));                    // skip the bullshit
+                    SetBranch(Ida2Mem(sprxInternalName, 0xAB04), Ida2Mem(sprxInternalName, 0xAD40));                    // skip the bullshit
+                    SetBranch(Ida2Mem(sprxInternalName, 0x3525C), Ida2Mem(sprxInternalName, 0x355B0));                    // skip everything except for malloc
+                    SetMem<uint32_t>(0x10021594, 0xDE7BBE46);           // xor value 2
+                    SetBranch(Ida2Mem(sprxInternalName, 0x2DBAC), Ida2Mem(sprxInternalName, 0x2DC24));                    // skip thread exit. ##Crashes sometimes if load too quickly or too slowly
 
-                vsh::ShowNotificationWithIcon(L"Paradox has been patched", vsh::NotifyIcon::Pen, vsh::NotifySound::Trophy);
+                    vsh::ShowNotificationWithIcon(L"Paradox has been patched", vsh::NotifyIcon::Pen, vsh::NotifySound::Trophy);
+                }
+
                 break;
             }
             case FindActiveGame::PatchedMenu::BO2Sprxio:
